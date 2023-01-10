dadd0a5331e8c628b8625fc64e1e5088a80b3102
NitramRednas <90842992+NitramRednas@users.noreply.github.com>
2023-01-10 09:40:19 +0100

    3 files modified

--- src/app/services/to-do-data.service.ts
+++ src/app/services/to-do-data.service.ts
@@ -7,6 +7,10 @@ export class ToDoDataService {
 
   constructor() { 
     this.todos.push(new ToDo('Einkaufen', false));
+    this.todos.push(new ToDo('Fleisch pökeln', true, '28.12.2022'));
+    this.todos.push(new ToDo('Hinterm Bahnhof prügeln', true));
+    this.todos.push(new ToDo('Essen', false, '18.02.2023'));
+    this.todos.push(new ToDo('Geschenke kaufen', true, '14.12.2022'));
   }
 
 }
